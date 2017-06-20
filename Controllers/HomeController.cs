4e5a6e6cee6fbb7d96abc8aab4286722acc5d706
GokGokalp <gok.gokalp@yahoo.com>
2017-06-20 23:25:40 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcore_graylog_sample_with_nlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            _logger.LogError("Something happened.");
+
+            return View();
+        }
+    }
+}
